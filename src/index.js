b9c50b3aa2b5b837dc87e138fbe9562410a06314
fershad <27988517+fershad@users.noreply.github.com>
2025-03-16 15:42:39 +1100

    initial branch implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { PowerBreakdown } from '@greenweb/grid-aware-websites';
 import { getLocation, saveDataToKv, fetchDataFromKv } from '@greenweb/gaw-plugin-cloudflare-workers';
 
+const thresholds = {
+	'low': 50,
+	'medium': 70,
+	'high': 100,
+}
+
 export default {
 	// First fetch the request
 	async fetch(request, env, ctx) {
@@ -10,6 +16,7 @@ export default {
 
 		// If the content is not HTML, then return the response without any changes.
 		if (!contentType || !contentType.includes('text/html')) {
+			console.log('Content type is not HTML');
 			return new Response(response.body, {
 				...response,
 			});
@@ -21,6 +28,7 @@ export default {
 			// This is useful for testing purposes. It can also be used to disable the feature for specific users.
 			const cookie = request.headers.get('cookie');
 			if (cookie && cookie.includes('gaw=false')) {
+				console.log('Grid-aware feature is disabled');
 				return new Response(response.body, {
 					...response,
 				});
@@ -33,6 +41,7 @@ export default {
 
 			// If the country data does not exist, then return the response without any changes.
 			if (!country) {
+				console.log('Country data does not exist');
 				return new Response(response.body, {
 					...response,
 				});
@@ -43,6 +52,7 @@ export default {
 
 			// If no cached data, fetch it using the PowerBreakdown class
 			if (!gridData) {
+				console.log('No cached data for country', country);
 				const options = {
 					mode: 'low-carbon',
 					apiKey: env.EMAPS_API_KEY,
@@ -53,6 +63,7 @@ export default {
 
 				// If there's an error getting data, return the web page without any modifications
 				if (gridData.status === 'error') {
+					console.log('Error getting grid data', gridData);
 					return new Response(response.body, {
 						...response,
 						headers: {
@@ -65,81 +76,92 @@ export default {
 				// By default data is stored for 1 hour.
 				await saveDataToKv(env, country, JSON.stringify(gridData));
 			} else {
+				console.log('Using cached data for country', country);
+
+				console.log('Grid data:', gridData);
 				// Parse twice because the data appears to be double-stringified
-				// TODO: Is this a bug with Wrangler, Workers KV, or something else?
-				gridData = JSON.parse(JSON.parse(gridData));
+				gridData = await JSON.parse(gridData);
 			}
 
+			let modifyHTML = new HTMLRewriter()
 
-			// If the grid aware flag is triggered (gridAware === true), then we'll return a modified HTML page to the user.
-			if (gridData.gridAware) {
-
-				// Use the HTMLRewriter API to modify the HTML page.
-				// We remove iframes that are present on the page.
-				// We also remove lazy loading from images that is done by the Smush plugin.
-				// We also remove the Youtube Lyte plugin.
-				// We also remove the Sendinblue plugin.
-				// We also add a banner to the top of the page to inform the user that the page is grid-aware.
-
-				const modifyHTML = new HTMLRewriter().on('iframe', {
+			// Check if the grid intensity is above the threshold
+			const powerPercentage = gridData.data.lowCarbonPercentage;
+			let intensity = 'unknown';
+			let logo = 'unknown';
+			if (powerPercentage < thresholds.low) {
+				intensity = 'low';
+				logo = 'green';
+				modifyHTML = modifyHTML.on('html', {
 					element(element) {
-						element.remove();
+						const style = element.getAttribute('style') || '';
+						element.setAttribute('style', style + '--bg-colour: #C8FF63; --hl-colour: #005C20; --body-colour: #1E1E1E;');
 					},
-				}).on('main', {
+				});
+
+				modifyHTML = modifyHTML.on('.entry-content .wp-block-image figure:not(.no-carbon) img, .entry-content figure.wp-block-image:not(.no-carbon) img, .entry-content figure.wp-block-gallery figure:not(.no-carbon) img', {
 					element(element) {
-						element.prepend(`<div class="grid-aware-info">
-							<div class="container single-page-container">
-							This page is grid-aware, and has been modified to use less power.
-							</div>
-							</div>`, { html: true });
-					},
-				}).on('body', {
-					element(element) {
-						element.prepend(`<style>
-							.grid-aware-info {
-								background-color: #f0f0f0;
-								padding-block: 0.5rem;
-								text-align: center;
-								color: #000;
-							}
-							</style>`, { html: true });
+						const style = element.getAttribute('style') || '';
+						element.setAttribute('style', style + 'display: initial !important;');
 					}
-				}).on('link[href*="code-prettify"]', {
+				});
+			} else if (powerPercentage < thresholds.medium) {
+				intensity = 'medium';
+				logo = 'blue';
+				const re = /(\d{4})\/(\d{2})\//gi;
+				modifyHTML = modifyHTML.on('html', {
 					element(element) {
-						element.remove();
+						const style = element.getAttribute('style') || '';
+						element.setAttribute('style', style + '--bg-colour: #87FEFF; --hl-colour: #00535C; --body-colour: #1E1E1E;');
 					},
-				}).on('script[src*="code-prettify"]', {
+				});
+
+				modifyHTML = modifyHTML.on('.entry-content .wp-block-image figure:not(.no-carbon) img, .entry-content figure.wp-block-image:not(.no-carbon) img, .entry-content figure.wp-block-gallery figure:not(.no-carbon) img', {
 					element(element) {
-						element.remove();
-					},
-				}).on('script[id*="code-prettify"]', {
-					element(element) {
-						element.remove();
-					},
-				}).on('script[id*="sib-front-js"]', {
-					element(element) {
-						element.remove();
-					},
-				}).on('script[src*="wp-youtube-lyte"]', {
-					element(element) {
-						element.remove();
+						const src = element.getAttribute('src');
+						src.replace(re, "$1/$2/low-res/");
+						const srcset = element.getAttribute('srcset');
+						srcset.replaceAll(re, "$1/$2/low-res/");
+						const style = element.getAttribute('style') || '';
+						element.setAttribute('style', style + 'display: initial !important;');
 					}
-			}).on('script[id*="smush-lazy-load-js"]', {
+				});
+
+				modifyHTML = modifyHTML.on('.entry-content .wp-block-image figure:not(.no-carbon) picture source, .entry-content figure.wp-block-image:not(.no-carbon) picture source, .entry-content figure.wp-block-gallery figure:not(.no-carbon) picture source', {
+					element(element) {
+						const srcset = element.getAttribute('srcset');
+						srcset.replaceAll(re, "$1/$2/low-res/");
+					}
+				});
+			} else if (powerPercentage <= thresholds.high) {
+				intensity = 'high';
+				logo = 'orange';
+				modifyHTML = modifyHTML.on('html', {
+					element(element) {
+						const style = element.getAttribute('style') || '';
+						element.setAttribute('style', style + '--bg-colour: #FFBF43; --hl-colour: #472E00; --body-colour: #1E1E1E;');
+					},
+				});
+
+
+			} else {
+				intensity = 'unknown';
+			}
+
+			modifyHTML = modifyHTML.on('.logo img', {
 				element(element) {
-					element.remove();
+					element.setAttribute('src', '/wp-content/themes/branch-theme/images/branch_' + logo + '-02.svg');
 				}
-			}).on('img[class*="wp-post-image lazyload"]', {
+			})
+
+
+			modifyHTML = modifyHTML.on('body', {
 				element(element) {
-					const src = element.getAttribute('data-src');
-					element.setAttribute('loading', 'lazy');
-					element.setAttribute('src', src);
-					element.removeAttribute('data-src');
-					element.removeAttribute('data-srcset');
-					element.removeAttribute('data-sizes');
-					element.removeAttribute('style');
-					element.setAttribute('class', "attachment-post-thumbnail size-post-thumbnail wp-post-image");
-				}
+					const classes = element.getAttribute('class') || '';
+					element.setAttribute('class', classes + ' ' + intensity + '-grid-intensity');
+				},
 			});
+
 
 				// Transform the response using the HTMLRewriter API, and set appropriate headers.
 				let modifiedResponse = new Response(modifyHTML.transform(response).body, {
@@ -151,15 +173,14 @@ export default {
 				});
 
 				return modifiedResponse;
-			}
 
-			// If the grid aware flag is not triggered, then we'll return the original HTML page to the user.
-			return new Response(response.body, {
-				...response,
-				headers: {
-					...response.headers,
-				},
-			});
+			// // If the grid aware flag is not triggered, then we'll return the original HTML page to the user.
+			// return new Response(response.body, {
+			// 	...response,
+			// 	headers: {
+			// 		...response.headers,
+			// 	},
+			// });
 		} catch (e) {
 
 			// If there's an error getting data, return the web page without any modifications
